80c614c66c73a7b7a2b0431fe97891cb4c03799d
Drake Arconis <drake@alchemyviewer.org>
2015-07-22 01:52:14 -0400

    Only use snprintf hack on pre-vc14 compilers

--- zlib/gzguts.h
+++ zlib/gzguts.h
@@ -46,7 +46,7 @@
 #  define NO_GZCOMPRESS
 #endif
 
-#if defined(STDC99) || (defined(__TURBOC__) && __TURBOC__ >= 0x550)
+#if defined(STDC99) || (defined(__TURBOC__) && __TURBOC__ >= 0x550) || (defined(_MSC_VER) && _MSC_VER >= 1900)
 #  ifndef HAVE_VSNPRINTF
 #    define HAVE_VSNPRINTF
 #  endif
@@ -99,7 +99,7 @@
    Microsoft more than a decade later!), _snprintf does not guarantee null
    termination of the result -- however this is only used in gzlib.c where
    the result is assured to fit in the space provided */
-#ifdef _MSC_VER
+#if (defined(_MSC_VER) && _MSC_VER < 1900)
 #  define snprintf _snprintf
 #endif
 
