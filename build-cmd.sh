3c24416d1967cfd7957d8ff23b55b8f22d11198b
Drake Arconis <drake@alchemyviewer.org>
2015-07-24 15:31:38 -0400

    Fix ones script missed

--- build-cmd.sh
+++ build-cmd.sh
@@ -38,7 +38,7 @@ pushd "$ZLIB_SOURCE_DIR"
             cmake -G"Visual Studio 14" .
 
             build_sln "contrib/vstudio/vc14/zlibvc.sln" "Debug" "Win32"
-            build_sln "contrib/vstudio/vc14/zlibvc.sln" "ReleaseWithoutAsm|Win32"
+            build_sln "contrib/vstudio/vc14/zlibvc.sln" "ReleaseWithoutAsm" "Win32"
             # conditionally run unit tests
             if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
                 build_sln "contrib/vstudio/vc14/zlibvc.sln" "Debug" "Win32" "testzlib"
@@ -75,7 +75,7 @@ pushd "$ZLIB_SOURCE_DIR"
             cmake -G"Visual Studio 14 Win64" .
 
             build_sln "contrib/vstudio/vc14/zlibvc.sln" "Debug" "x64" "zlibstat"
-            build_sln "contrib/vstudio/vc14/zlibvc.sln" "ReleaseWithoutAsm|x64" "zlibstat"
+            build_sln "contrib/vstudio/vc14/zlibvc.sln" "ReleaseWithoutAsm" "x64" "zlibstat"
 
             # conditionally run unit tests
             if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
